85fd6bb638006efd72d3d2da16e600b7fdd5eb7c
otuju004 <35447579+otuju004@users.noreply.github.com>
2018-01-16 13:20:35 +0200

    Added song lyrics

--- VSGitDemoA/VSGitDemoA/Program.cs
+++ VSGitDemoA/VSGitDemoA/Program.cs
@@ -13,7 +13,18 @@ namespace VSGitDemoA
                 string[] colours = { "Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", "White", "Black","Gray" };
                 Random rnd = new Random();
                 int index = rnd.Next(0, colours.Length);
-                Console.WriteLine("Colour of the day {0}", colours[index]);
+                Console.WriteLine("Yo listen up, here's the story
+About a little guy that lives in a {0} world
+And all day and all night and everything he sees is just {0}
+Like him, inside and outside
+{0} his house with a {0} little window
+And a {0} Corvette
+And everything is {0} for him
+And himself and everybody around
+'Cause he ain't got nobody to listen
+I'm {0} da ba dee da ba daa
+Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa
+Da ba dee da ba daa, da ba dee da ba daa, da ba dee da ba daa", colours[index]);
             }
         }
 }
