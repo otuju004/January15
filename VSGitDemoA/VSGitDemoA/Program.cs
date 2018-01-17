a83f4afd0b64a2b45fe05345112cb7d2c076c554
Student <Student@VMMB310-66>
2018-01-17 11:17:37 +0200

    zsa

--- VSGitDemoA/VSGitDemoA/Program.cs
+++ VSGitDemoA/VSGitDemoA/Program.cs
@@ -10,7 +10,7 @@ namespace VSGitDemoA
     {
        public static void Main()
             {
-                string[] colours = { "Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", "White", "Black","Gray" };
+                string[] colours = { "Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", "White", "Black","Gray"};
                 Random rnd = new Random();
                 int index = rnd.Next(0, colours.Length);
                 Console.WriteLine("Colour of the day {0}", colours[index]);
