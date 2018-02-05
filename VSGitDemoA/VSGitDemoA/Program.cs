a342aa40074532be120dcf248de09aee58b8985c
otuju004 <35447579+otuju004@users.noreply.github.com>
2018-02-05 11:32:06 +0200

    Added something for other project

--- VSGitDemoA/VSGitDemoA/Program.cs
+++ VSGitDemoA/VSGitDemoA/Program.cs
@@ -14,6 +14,20 @@ namespace VSGitDemoA
                 Random rnd = new Random();
                 int index = rnd.Next(0, colours.Length);
                 Console.WriteLine("Today's colour is {0}", colours[index]);
+           
+           //FOR ANOTHER PROJECT
+         //  new Weapon { weaponId=7, weaponName="Spear", damageBonus=2},
+//new Weapon { weaponId=8, weaponName="Halberd", damageBonus=2},
+//new Weapon { weaponId=9, weaponName="Atgeir", damageBonus=3},
+//new Weapon { weaponId=10, weaponName="Spiked Mace", damageBonus=3},
+//new Weapon { weaponId=11, weaponName="Iron Glove", damageBonus=2},
+//new Weapon { weaponId=12, weaponName="Druidic Staff", damageBonus=2},
+//new Weapon { weaponId=13, weaponName="Saarian Blade", damageBonus=2},
+//new Weapon { weaponId=14, weaponName="Ancient Axe", damageBonus=2},
+//new Weapon { weaponId=15, weaponName="Weaponized Pickaxe", damageBonus=1},
+//new Weapon { weaponId=16, weaponName="Skeleton Arm", damageBonus=1},
+
+
             }
         }
 }
